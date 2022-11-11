5d56e8508c406838778ac24791d72396e646ad7c
Aleksey <borbotal@yandex.ru>
2022-11-11 21:09:06 +0300

    task and ignore

---
+++ task00/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая 
+// на вход принимает число 
+// и выдаёт его квадрат
+
+// Например:
+// 4 -> 16
+// 3 -> 9
+// -7 -> 49
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int square = number * number;
+
+Console.WriteLine ($"квадрта числа {number} = {square}");
